030f51239fd7ce7c0cf9d7ea4b76eb2621069b55
rpage74 <rpque74@gmail.com>
2026-05-07 19:19:23 -0500

    prep for deploy

---
+++ src/test_htmlnode.py
@@ -0,0 +1,46 @@
+
+import unittest
+from htmlnode import HTMLNode, LeafNode, ParentNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_none(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_one(self):
+        node = HTMLNode(props={"href": "https://google.com"})
+        self.assertEqual(node.props_to_html(), ' href="https://google.com"')
+
+    def test_props_two(self):
+        node = HTMLNode(props={"href": "https://google.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://google.com" target="_blank"')
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_one(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_two(self):
+        node = LeafNode(None, "some text")
+        self.assertEqual(node.to_html(), "some text" )
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/test_textnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from textnode import TextNode, TextType, text_node_to_html_node
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_something_else(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_another_thing(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a DIFFERENT text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_more_things(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.example.com")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
